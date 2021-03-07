5371fccb129298c14627be6b265df617ea2173e3
Sebastian Alupoaei <sebastian-ioan.alupoaei@student.tuiasi.ro>
2021-03-07 20:39:55 +0200

    testare commit 2

--- src/rep_test/Test.java
+++ src/rep_test/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Folosire Repository GitHub");
 		System.out.println("heeei");
+		System.out.println("Test 2");
 
 	}
 
